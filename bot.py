4a4080281e0cfe08cc210fa0c64865a19d8f4da2
Jack Viper <sylosviper@gmail.com>
2020-06-04 06:03:34 +0000

    Initial commit

---
+++ bot.py
@@ -0,0 +1,55 @@
+import discord
+import random
+import sys
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix='$', description='Waddup')
+
+
+@bot.event
+async def on_ready():
+    print('logged in as: {}'.format(bot.user.name))
+    print('bot ID: {}'.format(bot.user.id))
+    print('----')
+
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Welcome!")
+
+@bot.command()
+async def shutdown(ctx):
+    await ctx.send("Killing myself!")
+    sys.exit('Killing Bot')
+
+@bot.command()
+async def add(ctx, a: int, b: int):
+    await ctx.send(a+b)
+
+@bot.command()
+async def multiply(ctx, a:int, b: int):
+    await ctx.send(a*b)
+
+@bot.command()
+async def cat(ctx):
+    await ctx.send('Meow! =^.^=')
+
+@bot.command()
+async def roll(ctx, a:str):
+    num_dice = float(a.split('d')[0])
+    dice_type = float(a.split('d')[1])
+    random_value = 0
+
+    for x in range(int(num_dice)):
+        random_value += random.randint(1, int(dice_type))
+
+    await ctx.send("You rolled: {}".format(random_value))
+
+@bot.command()
+async def command_help(ctx):
+    commands =  """```
+    commands: greet, add, multiply, cat, roll
+    ```"""
+    await ctx.send(commands)
+
+
+bot.run('NDY2NDIyOTg5NTI1ODc2NzM2.DjAu1w.3393qog5bqEQs-o8QiTVNpJXHrc')
