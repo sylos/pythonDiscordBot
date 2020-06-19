8c74d6ac5933e55c4318e8131edafd1459b46fe7
Jack Viper <sylosviper@gmail.com>
2020-06-19 22:00:28 +0000

    I don't remember. I messed this commit up

--- bot.py
+++ bot.py
@@ -3,11 +3,14 @@
 import discord
 import random
 import sys
+import os
 import simple_commands
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix='$', description='Waddup')
+BOT_TOKEN = 'NDY2NDIyOTg5NTI1ODc2NzM2.DjAu1w.3393qog5bqEQs-o8QiTVNpJXHrc'
 BOT_OWNER_ID = 118907310243315712
+
 
 @bot.event
 async def on_ready():
@@ -17,16 +20,29 @@ async def on_ready():
 
 @bot.command()
 async def greet(ctx):
-    await ctx.send(":smiley: :wave: Welcome!")
+    user =  await bot.fetch_user(ctx.message.author.id)
+    await ctx.send(":smiley: :wave: Welcome! {}".format(user.name))
 
 @bot.command()
 async def shutdown(ctx):
     if ctx.message.author.id == BOT_OWNER_ID:   
         await ctx.send("Killing myself!")
         sys.exit('Killing Bot')
-    
+#log this    
     await ctx.send("""Killing myself!  Haha...just kidding.  
-            {} is not authorized""".format(ctx.message.author.id))
+            {} {} is not authorized""".format(ctx.message.author.name,ctx.message.author.id))
+
+@bot.command()
+async def restart(ctx):
+    if ctx.message.author.id == BOT_OWNER_ID:
+        await ctx.send("Taking a nap then waking!")
+        print("Restarting: {}".format(sys.argv))
+        print("Running py venv: {}".format(sys.executable))
+        os.execl(sys.executable,sys.executable, *sys.argv)
+    #log this with author
+    await ctx.send("I don't wanna!")
+
+
 
 @bot.command()
 async def add(ctx, a: int, b: int):
@@ -36,7 +52,7 @@ async def add(ctx, a: int, b: int):
 async def multiply(ctx, a:int, b: int):
     await ctx.send(a*b)
 
-@bot.command()
+@bot.command(name='author')
 async def authorID(ctx):
     await ctx.send("author is: {}".format(
         ctx.message.author.id))
@@ -63,5 +79,10 @@ async def command_help(ctx):
     ```"""
     await ctx.send(commands)
 
+def main():
+    bot.load_extension('simple_commands')
+    bot.run(BOT_TOKEN)
 
-bot.run('NDY2NDIyOTg5NTI1ODc2NzM2.DjAu1w.3393qog5bqEQs-o8QiTVNpJXHrc')
+
+if __name__=='__main__':
+    main()
--- simple_commands.py
+++ simple_commands.py
@@ -1,7 +1,41 @@
-import discord
 from discord.ext import commands
 
-@commands.command()
-async def test(ctx):
-    await ctx.send("this test worked")
+
+class Simple_commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.active_number_game = False
+
+    
+    @commands.command()
+    async def chanUsers(self, ctx):
+        await ctx.send("Channel Users: {}".format(ctx.channel.members.name))
+
+    @commands.command()
+    async def repeat(self,ctx,*, a: str="No strings on me"):
+        await ctx.send("You said: {}".format(a))
+
+    @commands.command(name="numbers")
+    async def guess_number(self, ctx):
+        number = 0
+        await ctx.send("Game is active: {}".format(self.active_number_game))
+        if (self.active_number_game):
+            self.active_number_game = not self.active_number_game
+            await ctx.send("We have an active game")
+        else:   
+            await ctx.send("We shall play Guess the Number!")
+            self.active_number_game = not self.active_number_game
+            await ctx.send("Number is: {}".format(number))
+       
+        await ctx.send("Game is active: {}".format(self.active_number_game))
+    
+    @commands.command(name="quote")
+    async def quote_user(self, ctx, *, a: str="No Strings On Me"):
+        await ctx.send("You said: {}".format(a))
+def setup(bot):
+  #     print('Loading test')
+    bot.add_cog(Simple_commands(bot))
+      #  bot.add_command(chanUsers)
+      #  bot.add_command(repeat)
+
 
